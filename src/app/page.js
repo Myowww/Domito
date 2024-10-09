e38e4cc42b9d365a77360de744ec37e506e961f8
myowww <eahenlee@gmail.com>
2024-10-10 08:24:03 +0900

    드디어디자인적ㄷ용된다!!

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+import './globals.css';
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-8">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],  
+  content: ['./src/**/*.{js,jsx,ts,tsx}'], // Tailwind CSS가 적용될 파일 경로
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+  plugins: [],
+}
+
